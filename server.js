c2e1e1ad2231094c3e30668f0ac5a4ac41426a0a
alex mendoza <alexandermen2017@gmail.com>
2023-03-21 19:27:45 -0400

    made this project work with node.js made a public folder

---
+++ server.js
@@ -0,0 +1,14 @@
+//setting up node js 
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const glob = require("glob")
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.static('./public'));
+
+
+app.listen(5680); //start the server
+console.log('Server is running...');
